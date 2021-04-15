f5dc6c6a17d4db1209e18bcc9297834c4b824875
Anthony Pham <khanhdinhpham@gmail.com>
2021-04-15 13:11:01 -0700

    Update: About page goal

--- client/src/views/pages/About.js
+++ client/src/views/pages/About.js
@@ -45,7 +45,20 @@ function About() {
 				<div className="d-flex justify-content-between pt-5">
 					<div className="px-5 ml-5">
 						<h1>Our goal</h1>
-						<p></p>
+						<p>
+							Not only is Chinatown a location close to many of us
+							here in Vancouver, but it is in fact the site of
+							many recent anti-Asian hate crimes as well. In fact,
+							the climax of our artwork experience centers around
+							recreating a true event in the very same location in
+							the virtual city block as it did in real life. The
+							primary conceptual goal of our artwork is
+							ideological in nature, and is to reveal the
+							seriousness and ever-growing scale of the issue of
+							anti-Asian racist incidents especially in the wake
+							of COVID-19, in a manner that might put the user in
+							the shoes of an Asian person, if only for a moment.
+						</p>
 					</div>
 					<div className="px-5"></div>
 					<div className="px-5 mr-5">
