7fbf574d12af307348b21b6f2f6a79d8c3cf8953
Anthony Pham <khanhdinhpham@gmail.com>
2021-04-15 12:42:54 -0700

    Server

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const cors = require('cors');
 const router = require("./routes/index");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
 
 const app = express();
 const PORT = 3001;
-const MONGODB_URI =
-	"mongodb+srv://anthony:iat@newcluster.bcgbl.mongodb.net/iat222-project?retryWrites=true&w=majority";
 
 // app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", router);
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true,
@@ -24,6 +25,14 @@ mongoose.connection.once("open", function () {
 mongoose.connection.on("error", function (error) {
 	console.log("Mongoose Connection Error : " + error);
 });
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(PORT, function () {
 	console.log(`Server listening on port ${PORT}.`);
